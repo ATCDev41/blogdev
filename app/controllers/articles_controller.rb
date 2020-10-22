fe487995b4bbed6f50cd679e2c16cd5b59de8764
ATCDev41 <alex.atc.digital@gmail.com>
2020-10-22 02:31:45 +0200

    Edit / no edit done + message validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ def new
   @article = Article.new
 end
 
+def edit
+  @article = Article.find(params[:id])
+end
+
 def create
   @article = Article.new(article_params)
   if @article.save
@@ -12,6 +16,18 @@ def create
   else
     render 'new'
   end
+end
+
+def update
+  @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = " article modifié bro"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+end
+
+
 
 end
 
@@ -26,6 +42,4 @@ private
   def article_params
     params.require(:article).permit(:title, :description)
   end
-
-
 end
