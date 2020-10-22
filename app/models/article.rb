b90040a3670e8787a792588e003e787a025a425c
ATCDev41 <alex.atc.digital@gmail.com>
2020-10-22 02:17:24 +0200

    front new/show/errors

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
 
-  validates :title, presence:true, length (minimum: 5, maximum: 100)
-    validates :description, presence:true, length (minimum: 5, maximum: 1000)
+  validates :title, presence: true, length: {minimum: 3, maximum: 100}
+  validates :description, presence: true, length: {minimum: 3, maximum: 1000}
 end
